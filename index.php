81088f388281d55d7ef25e36654690e9dac09dc5
Charlesrcm <charlesrc@outlook.fr>
2022-04-28 15:35:11 +0200

    formulaire inscrip

---
+++ index.php
@@ -0,0 +1,13 @@
+<form action="" method="post">
+  <label for="name">Nom : </label>
+  <input type="text" name="name" id="name">
+
+  <label for="surname">Prénom : </label>
+  <input type="text" name="surname" id="surname">
+
+  <label for="mail">Adresse mail : </label>
+  <input type="text" name="mail" id="mail">
+
+  <label for="pass">Mot de passe : </label>
+  <input type="text" name="pass" id="pass">
+</form>
