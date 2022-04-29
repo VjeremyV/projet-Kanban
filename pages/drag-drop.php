890941f944b21a363339a1194e4ed5caa151f604
Charlesrcm <charlesrc@outlook.fr>
2022-04-29 12:34:01 +0200

    test1

--- pages/drag-drop.php
+++ pages/drag-drop.php
@@ -1,6 +1,8 @@
 <div id="container">
-  <p>Ajoutez un élément</p>
-  <div draggable="true" class="box">A</div>
+  <div>
+    <p>Ajoutez un élément</p>
+    <div draggable="true" class="box">A</div>
+  </div>
   <div draggable="true" class="box">B</div>
   <div draggable="true" class="box">C</div>
 </div>
