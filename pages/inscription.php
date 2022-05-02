9c865f761c97ff838f9402381969411ccb97ea44
christopherfordant <90460339+christopherfordant@users.noreply.github.com>
2022-05-02 16:36:28 +0200

    merge developp

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,24 +1,26 @@
-<?php
- echo '<form action="?inscription=true" method="POST">
+
+ echo '<form  class = "form-group d-flex flex-column justify-content center  align-items center" action="?inscription=true" method="POST">
+ <div class="form-group">
  <label for="name">Nom :</label>
-     <input type="text" name="name" id="name" >
+     <input type="text" class = "form-control" name="name" id="name" >
  
      <label for="surname">Prénom :</label>
      <input type="text" name="surname" id="surname" >
  
      <label for="mail">Adresse mail :</label>
-     <input type="mail" name="mail" id="mail" >
+     <input type="mail"  class="form-control" name="mail" id="mail" >
  
      <label for="pass">Mot de passe :</label>
-     <input type="password" name="pass" id="pass" >
+     <input type="password"  class="form-control" name="pass" id="pass" >
  
      <label for="realpass">Confirmez votre mot de passe :</label>
-     <input type="password" name="realpass" id="realpass" >
+     <input type="password" class="form-control" name="realpass" id="realpass" >
  
      <label for="file">Ajoutez une photo de profil :</label>
-     <input type="file" name="file" id="file">
+     <input type="file" class="form-control" name="file" id="file">
 
      <div class="g-recaptcha" data-sitekey="6LeW3LMfAAAAAMugN2owWR3iIAJVqOfjfebFecZc"></div>
  
  <input type="submit" value="s\'incrire" name="submit">
- </form>';
+ <div class="form-group">
+<span id = "barre"></span></form>';
