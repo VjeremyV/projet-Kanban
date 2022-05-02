144a36e1fce0b976945e5fc911f20db97ba32976
Charlesrcm <charlesrc@outlook.fr>
2022-05-02 22:39:38 +0200

    push de 22:40

--- template/header.php
+++ template/header.php
@@ -1,58 +1,60 @@
-<?php include_once(__DIR__.'/../src/fonctions/security.php');
-include_once(__DIR__.'/../src/fonctions/affichage.php');
+<?php include_once(__DIR__ . '/../src/fonctions/security.php');
+include_once(__DIR__ . '/../src/fonctions/affichage.php');
 ?>
 
 <body>
-<header class="container" role="banner">
+    <header class="container" role="banner">
 
 
 
 
-<div class="col d-flex justify-content-between align-items-center navContainer bg-light  px-lg-3">
 
-<a href="./../index.php" class=" col-lg-4 d-flex align-items-start text-decoration-none flex-column">
-    <img src="./../style/assets/tretrello_logo.webp" class="image" alt="logo-tretrello">
-    <p class="text-primary fw-bold fs-3 ms-1 slogan">C'est très très bien</p>
-</a>
-<?php
-// Si connecter alors afficher les projets, le profil et le déconnexion
-                    if (isConnect()) : //* A voir pour valider la connexion
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-around">
+            <div class="d-flex justify-content-around align-items-center bg-light px-lg-3">
+                <a href="./../index.php" class="text-decoration-none flex-column">
+                    <img src="./../style/assets/tretrello_logo.webp" class="image" alt="logo-tretrello">
+                    <p class="text-primary fw-bold fs-3 ms-1 slogan">C'est très très bien</p>
+                </a>
+                <?php
+                // Si connecter alors afficher les projets, le profil et le déconnexion
+                if (isConnect()) : //* A voir pour valider la connexion
                 ?>
-<nav class=" col-lg-8 d-none d-lg-block">
-    <ul class="d-flex text-black-50 fw-bold justify-content-around fs-5 mt-3">
-        <li><a class='link-<?= isset($_GET['page']) && $_GET['page']== "encours" ? "warning" : "primary"?>' <?= isset($_GET['page']) && $_GET['page']== "encours" ? "title='Lien actif'" : ""?>href="../pages/projets.php?page=encours" >Projet en cours</a></li>
-        <li><a class="link-<?= isset($_GET['page']) && $_GET['page']== "terminer_projet" ? "warning" : "primary"?>" <?= isset($_GET['page']) && $_GET['page']== "encours" ? "title='Lien actif'" : ""?>href="../pages/projets.php?page=terminer_projet" >Projet Finis</a></li>
-        <li><a class="link-<?= isset($_GET['page']) && $_GET['page']== "creerprojet" ? "warning" : "primary"?>" <?= isset($_GET['page']) && $_GET['page']== "encours" ? "title='Lien actif'" : ""?>href="../pages/createprojet.php?creation=0&page=creerprojet"> Créer Projet</a></li>
-        <li><a class="link-<?= isset($_GET['page']) && $_GET['page']== "profil" ? "warning" : "primary"?>"<?= isset($_GET['page']) && $_GET['page']== "encours" ? "title='Lien actif'" : ""?> href="index.php?page=profil" >Profil</a></li>
-        <li><a class="link-primary" href="./../pages/deconnexion.php">Se déconnecter</a></li>
-    </ul>
-</nav>
+                    <div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link border-bottom border-info <?= isset($_GET['page']) && $_GET['page'] == "encours" ? "warning" : "primary" ?>" <?= isset($_GET['page']) && $_GET['page'] == "encours" ?: "" ?>href="/../pages/projets.php?page=encours">Projet en cours</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link border-bottom border-info <?= isset($_GET['page']) && $_GET['page'] == "terminer_projet" ? "warning" : "primary" ?>" <?= isset($_GET['page']) && $_GET['page'] == "encours" ?: "" ?>href="/../pages/projets.php?page=terminer_projet">Projet Finis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link border-bottom border-info <?= isset($_GET['page']) && $_GET['page'] == "creerprojet" ? "warning" : "primary" ?>" <?= isset($_GET['page']) && $_GET['page'] == "encours" ?: "" ?>href="/../pages/createprojet.php?creation=0&page=creerprojet"> Créer Projet</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link border-bottom border-info <?= isset($_GET['page']) && $_GET['page'] == "profil" ? "warning" : "primary" ?>" <?= isset($_GET['page']) && $_GET['page'] == "encours" ?: "" ?> href="index.php?page=profil">Profil</a>
+                    </li>
+                    <li>
+                        <a class="nav-link border-bottom border-info" href="./../pages/deconnexion.php">Se déconnecter</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-<a class="nav-link dropdown-toggle d-lg-none text-success" href="#" id="navbarDropdownMenuLink"
-    role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-        class="bi bi-list text-success" viewBox="0 0 16 16">
-        <path fill-rule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-    </svg>
-</a>
-
-<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-    <li><a class="dropdown-item" href="../pages/projets.php?page=encours" >Projet en cours</a></li>
-    <li><a class="dropdown-item" href="../pages/projets.php?page=terminer_projet" >Projet Finis</a></li>
-    <li><a class="dropdown-item" href="../pages/createprojet.php?creation=0&page=creerprojet"> Créer Projet</a></li>
-    <li><a class="dropdown-item" href="index.php?page=profil" >Profil</a></li>
-    <li><a class="dropdown-item" href="./../pages/deconnexion.php">Se déconnecter</a></li>
-</ul>
-<?php
-                    endif;
-                 ?>
-</div>
+    <?php
+                endif;
+    ?>
     <h1>
- <!-- je change le titre en fonction de la page sur laquelle je suis -->
-         <?php
-       setContent('Accueil - Tretrello', 'Inscription à Tretrello', 'Gestion du profil', 'Vos Projet en cours','Créer un nouveau projet', 'Vos Projets terminés');
-         ?> 
+        <!-- je change le titre en fonction de la page sur laquelle je suis -->
+        <?php
+        setContent('Accueil - Tretrello', 'Inscription à Tretrello', 'Gestion du profil', 'Vos Projet en cours', 'Créer un nouveau projet', 'Vos Projets terminés');
+        ?>
     </h1>
-</header>
-<main class="container">
+    </header>
+    <main class="container">
