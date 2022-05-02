f3c64553110dee5515d669f3a3beef49a0719e3a
Charlesrcm <charlesrc@outlook.fr>
2022-05-02 15:36:37 +0200

    choix de deux versions du form d'inscription

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,46 +1,51 @@
 <?php
 echo '
-<div class="d-md-flex mt-4">
+<div class="d-md-flex mt-4 col-md-11">
  <form action="?inscription=true" method="POST" class="flex-md-columns col-md-6">
 
- <div class="d-flex my-2 justify-content-between me-0 justify-content-md-between">
-    <label for="name" class="form-label pe-2">Nom :</label>
-     <input type="text" name="name" id="name"  class="form-control-sm">
+<div class="form-floating">
+ <input type="text" name="name" id="name" class="form-control" placeholder="Nom">
+ <label for="name" class="">Nom</label>
+</div>
+
+<div class="form-floating">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
 </div>
  
-<div class="d-flex my-2 justify-content-between justify-content-md-between">
+<div class="d-flex my-2 justify-content-between">
      <label for="surname" class="form-label pe-2">Prénom :</label>
      <input type="text" name="surname" id="surname"  class="form-control-sm">
 </div>
 
-<div class="d-flex my-2 justify-content-between justify-content-md-between">
+<div class="d-flex my-2 justify-content-between">
      <label for="mail" class="form-label pe-2">Adresse mail :</label>
      <input type="mail" name="mail" id="mail"  class="form-control-sm">
 </div>
 
-<div class="d-flex my-2 justify-content-between justify-content-md-between">
+<div class="d-flex my-2 justify-content-between">
      <label for="pass" class="form-label pe-2">Mot de passe :</label>
      <input type="password" name="pass" id="pass"  class="form-control-sm">
 </div> 
 
-<div class="d-flex my-2 justify-content-between justify-content-md-between">
+<div class="d-flex my-2 justify-content-between">
      <label for="realpass" class="form-label pe-2">Confirmez votre mot de passe :</label>
      <input type="password" name="realpass" id="realpass"  class="form-control-sm">
 </div> 
 
-<div class="d-flex my-2 justify-content-between justify-content-md-between">
+<div class="d-flex my-2 justify-content-between">
      <label for="file" class="form-label pe-2">Ajoutez une photo de profil :</label>
      <input type="file" name="file" id="file" class="form-control-sm" multiple>
 </div>
 
-     <div class="g-recaptcha" data-sitekey="6LeW3LMfAAAAAMugN2owWR3iIAJVqOfjfebFecZc"></div>
+     <div class="g-recaptcha mt-5 my-mt-0" data-sitekey="6LeW3LMfAAAAAMugN2owWR3iIAJVqOfjfebFecZc"></div>
  
-<div>
+<div class="d-flex justify-content-center justify-content-md-end mt-3">
  <input type="submit" value="s\'incrire" name="submit" class="btn btn-primary">
 </div>
  </form>
 
- <span class="border-end border-dark border-5 rounded mx-md-5"></span>
+ <span class="border-end border-dark border-5 rounded mx-md-5 bg-dark d-none d-md-flex"></span>
 
 <div class="d-flex col-md-6 mt-4 mt-md-0"> 
  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
