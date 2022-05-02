c32c6bb6bf1c40a30669a867b2a617d0f164e4fb
Jeremy <vaugoyeau.jeremy@gmail.com>
2022-05-02 17:56:16 +0200

    finalisation partie kanban

--- pages/projet.php
+++ pages/projet.php
@@ -9,7 +9,7 @@ if (isConnect()) {
     try {
         $dbh = new PDO('mysql:host=localhost;dbname=tretrello', 'tretrello', 'tretrello', array(PDO::ATTR_PERSISTENT => true));
         include_once(__DIR__ . './../src/fonctions/draggable.php');
-        include_once(__DIR__ . '/../src/fonctions/modal.php');
+        @include_once(__DIR__ . '/../src/fonctions/modal.php');
         include_once(__DIR__ . '/../src/fonctions/categoriesPosition.php');
 
 
@@ -24,7 +24,7 @@ if (isConnect()) {
             echo "<p>Une erreur est survenue lors de la connexion à la base de données</p>";
         }
         ?>
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-evenly flex-wrap">
             <?php
             $stmt = $dbh->prepare('SELECT `nom_categories`,`id_categorie_categories`,`ordre` FROM categories join projet on categories.id_projet_projet = projet.id_projet_projet WHERE projet.id_projet_projet = :idProjet ORDER BY ordre');
             if ($stmt->execute(['idProjet' => $idProjet])) {
@@ -40,12 +40,13 @@ if (isConnect()) {
                     foreach ($resultats as $resultat) {
                         $categories[] = [$resultat['nom_categories'] => $resultat['id_categorie_categories']];
             ?>
-   
-                        <div class="categorieContainer px-1 pb-5" data-id="<?= $resultat['id_categorie_categories'] ?>" style ="order :<?= $resultat['ordre']?> ;">
 
-                            <h2><?php if($resultat['ordre'] > 1):?><button class="fs-5 px-2 mx-2 btn border boutonMoins"><</button><?php endif;?><?= $resultat['nom_categories'] ?><?php if($resultat['ordre'] < count($resultats)):?> <button class="fs-5 px-2 mx-2 btn border boutonPlus">></button><?php endif;?></h2>
-                            
-                                <ul data-draggable="target" id=<?= $resultat['id_categorie_categories'] ?>>
+                        <div class="categorieContainer px-1 pb-5" data-id="<?= $resultat['id_categorie_categories'] ?>" style="order :<?= $resultat['ordre'] ?> ;">
+
+                            <h2><?php if ($resultat['ordre'] > 1) : ?><button class="fs-5 px-2 mx-2 btn border boutonMoins"><</button><?php endif; 
+                                ?><?= $resultat['nom_categories'] ?><?php if ($resultat['ordre'] < count($resultats)) : ?> <button class="fs-5 px-2 mx-2 btn border boutonPlus">></button><?php endif; ?></h2>
+
+                            <ul data-draggable="target" id=<?= $resultat['id_categorie_categories'] ?>>
                                 <?php for ($j = 0; $j < count($res); $j++) {
                                     if (in_array($resultat['id_categorie_categories'], $res[$j])) {
                                 ?>
@@ -97,7 +98,7 @@ if (isConnect()) {
 
                                                 <button class="btn btn-danger mb-3" data-target='#suppr-<?= $res[$j]['id_taches_taches'] ?>' data-toggle='modal'>supprimer</button>
                                                 <form action="" method="post" class="modal px-5 w-25 h-25 mx-auto my-auto rounded text-light d-flex flex-column justify-content-center" id="suppr-<?= $res[$j]['id_taches_taches'] ?>">
-                                                    <label class="m-3" for="suppression">Confirmez-vous la suppresion de la tâche ?</label>
+                                                    <label class="fs-4 m-3" for="suppression">Confirmez-vous la suppresion de la tâche ?</label>
                                                     <div class="d-flex justify-content-center suppr-buttons">
                                                         <input class="m-3" type="submit" value="Oui" name="suppression">
                                                         <input type="hidden" name="supprId" value="<?= $res[$j]['id_taches_taches'] ?>">
@@ -111,7 +112,40 @@ if (isConnect()) {
                                 }
                                 ?>
                             </ul>
-                            <button class="btn btn-primary addTache align-self-end" data-target='#modal-new-<?= $resultat['id_categorie_categories'] ?>' data-toggle=modal>+</button>
+                            <div class="d-flex justify-content-between barreTache px-2 w-100">
+                                <button class="btn-danger" data-target='#modal-suppr-<?= $resultat['id_categorie_categories'] ?>' data-toggle=modal><i class="fa-solid fa-trash"></i></button>
+                                <button class="btn btn-primary fw-bold addbutton" data-target='#modal-new-<?= $resultat['id_categorie_categories'] ?>' data-toggle=modal>+</button>
+                            </div>
+                            <div class="modal" id="modal-new-categorie" role="dialog">
+                                <div class="mw-100 bg-light mx-auto my-auto rounded">
+                                    <form action="" method="post">
+                                        <div class="p-3 border-bottom border-dark d-flex flex-column justiy-content-center align-items-start">
+                                            <label for="newCategorie">Intitulé de la Catégorie</label>
+                                            <input type="text" id='newCategorie' name='newCategorie' class="h3"></input>
+                                        </div>
+                                        <input type="hidden" name="idProjet" value="<?= $_GET['id'] ?>">
+                                        <div class="border-top border-dark px-5 py-3 fs-5 d-flex ">
+                                            <button role="button" class="btn btn-danger m-2" data-dismiss="dialog">Fermer</button>
+                                            <input type="submit" class="btn btn-success m-2" value="Envoyer">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div class="modal" id="modal-suppr-<?= $resultat['id_categorie_categories'] ?>" role="dialog">
+                                <div class="mw-100 text-light mx-auto my-auto rounded">
+                                    <form action="" method="post">
+                                        <div class="p-3 border-bottom border-dark d-flex flex-column justiy-content-center align-items-start">
+                                            <input type="hidden" name="idCat" value="<?= $resultat['id_categorie_categories'] ?>">
+                                            <label class="fs-4" for="supprCat">Confirmez-vous la suppresion de la catégorie ? <br>Toutes les tâches et commentaires affiliés seront supprimés</label>
+                                            <div class="d-flex justify-content-center w-100 suppr-buttons">
+                                                <input class="m-3" type="submit" name="supprCat" id="supprCat" value="Oui">
+                                                <button class="m-3" role="button" data-dismiss="dialog">Non</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
 
                             <div class="modal" id="modal-new-<?= $resultat['id_categorie_categories'] ?>" role="dialog">
                                 <div class="mw-100 bg-light mx-auto my-auto rounded">
@@ -151,21 +185,7 @@ if (isConnect()) {
         <div class="d-flex justify-content-end mt-3">
             <button class="btn btn-primary p-2" data-target='#modal-new-categorie' data-toggle=modal>Ajouter une catégorie</button>
         </div>
-        <div class="modal" id="modal-new-categorie" role="dialog">
-            <div class="mw-100 bg-light mx-auto my-auto rounded">
-                <form action="" method="post">
-                    <div class="p-3 border-bottom border-dark d-flex flex-column justiy-content-center align-items-start">
-                        <label for="newCategorie">Intitulé de la Catégorie</label>
-                        <input type="text" id='newCategorie' name='newCategorie' class="h3"></input>
-                    </div>
-                    <input type="hidden" name="idProjet" value="<?= $_GET['id']?>">
-                    <div class="border-top border-dark px-5 py-3 fs-5 d-flex ">
-                        <button role="button" class="btn btn-danger m-2" data-dismiss="dialog">Fermer</button>
-                        <input type="submit" class="btn btn-success m-2" value="Envoyer">
-                    </div>
-                </form>
-            </div>
-        </div>
+
     <?php
 
     include_once(__DIR__ . '/../template/footer.php');
--- src/fonctions/modal.php
+++ src/fonctions/modal.php
@@ -1,5 +1,5 @@
 <?php
-include_once(__DIR__.'./formulaire.php');
+include_once(__DIR__ . '/formulaire.php');
 if (isset($_POST['newTache']) && !empty($_POST['newTache'])) {
     $newTache = htmlentities($_POST['newTache']);
     $newDescription = $_POST['newDescription'];
@@ -12,7 +12,7 @@ if (isset($_POST['newTache']) && !empty($_POST['newTache'])) {
 if (isset($_POST['nomTache']) && isset($_POST['description']) && isset($_POST['date']) && isset($_POST['idTache'])) {
     $nomTache = htmlentities($_POST['nomTache']);
     $description = htmlentities($_POST['description']);
-    if(validateDate($_POST['date'], 'Y-m-d')){
+    if (validateDate($_POST['date'], 'Y-m-d')) {
         $date = htmlentities($_POST['date']);
         $idTache = $_POST['idTache'];
         $stmt = $dbh->prepare('UPDATE taches set `nom_taches`= :nom, `date_taches`= :date, `description_taches` = :description WHERE `id_taches_taches` = :idTache');
@@ -20,20 +20,20 @@ if (isset($_POST['nomTache']) && isset($_POST['description']) && isset($_POST['d
             echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
         }
     }
-    if(isset($_POST['com']) && !empty($_POST['com'])){
+    if (isset($_POST['com']) && !empty($_POST['com'])) {
         $commentaire = htmlentities($_POST['com']);
         $stmt = $dbh->prepare('insert into commentaires (`date_commentaires`, `texte_commentaires`,`id_utilisateur_utilisateur`,`id_taches_taches`) values (:dateCom, :textCom, :idUser, :idTache )');
-        if(!$stmt->execute(['dateCom' => date('Y-m-d'), 'textCom' => $commentaire, 'idUser' => $_SESSION['id'], 'idTache' => $idTache])){
+        if (!$stmt->execute(['dateCom' => date('Y-m-d'), 'textCom' => $commentaire, 'idUser' => $_SESSION['id'], 'idTache' => $idTache])) {
             echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
         }
     }
 }
 
-if(isset($_POST['suppression']) && isset($_POST['supprId'])){
-    $stmt=$dbh->prepare('DELETE FROM commentaires WHERE `id_taches_taches` = :idTache');
-    if($stmt->execute(['idTache' => $_POST['supprId']])){
+if (isset($_POST['suppression']) && isset($_POST['supprId'])) {
+    $stmt = $dbh->prepare('DELETE FROM commentaires WHERE `id_taches_taches` = :idTache');
+    if ($stmt->execute(['idTache' => $_POST['supprId']])) {
         $stmt = $dbh->prepare('DELETE FROM taches WHERE `id_taches_taches` = :idTache');
-        if(!$stmt->execute(['idTache' => $_POST['supprId']])){
+        if (!$stmt->execute(['idTache' => $_POST['supprId']])) {
             echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
         }
     } else {
@@ -41,13 +41,37 @@ if(isset($_POST['suppression']) && isset($_POST['supprId'])){
     }
 }
 
-if(isset($_POST['newCategorie']) && !empty($_POST['newCategorie'])){
+if (isset($_POST['newCategorie']) && !empty($_POST['newCategorie'])) {
     $newCat = htmlentities($_POST['newCategorie']);
     $stmt = $dbh->prepare('select Count(*) from categories Where `id_projet_projet` = :idProjet');
-    if($stmt->execute(['idProjet' => $_GET['id']])){
+    if ($stmt->execute(['idProjet' => $_GET['id']])) {
         $nbreCat = $stmt->fetch(PDO::FETCH_NUM);
-        $stmt=$dbh->prepare('insert into categories (`nom_categories`, `id_projet_projet`,`id_utilisateur_utilisateur`, `ordre`) VALUES (:nomCat, :idProjet, :idUser, :ordre)');
-        if(!$stmt->execute(['nomCat' => $newCat, 'idProjet' => $_GET['id'], 'idUser' => $_SESSION['id'], 'ordre' => $nbreCat[0]+1])){
+        $stmt = $dbh->prepare('insert into categories (`nom_categories`, `id_projet_projet`,`id_utilisateur_utilisateur`, `ordre`) VALUES (:nomCat, :idProjet, :idUser, :ordre)');
+        if (!$stmt->execute(['nomCat' => $newCat, 'idProjet' => $_GET['id'], 'idUser' => $_SESSION['id'], 'ordre' => $nbreCat[0] + 1])) {
+            echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
+        }
+    } else {
+        echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
+    }
+}
+
+if (isset($_POST['supprCat']) && isset($_POST['idCat'])) {
+    $stmt = $dbh->prepare('DELETE commentaires FROM commentaires join taches on commentaires.id_taches_taches = taches.id_taches_taches WHERE taches.id_categorie_categories = :idCat');
+    if ($stmt->execute(['idCat' => $_POST['idCat']])) {
+        $stmt = $dbh->prepare('DELETE FROM taches WHERE `id_categorie_categories` = :idCat');
+        if ($stmt->execute(['idCat' => $_POST['idCat']])) {
+            $stmt = $dbh->prepare('select ordre from categories where `id_categorie_categories` = :idcat');
+            if ($stmt->execute(['idcat' => $_POST['idCat']])) {
+                $cats = $stmt->fetchall(PDO::FETCH_NUM)[0][0];
+                $stmt = $dbh->prepare('UPDATE categories set ordre=ordre-1 WHERE ordre > :orderCat;');
+                if ($stmt->execute(['orderCat' => $cats])) {
+                    $stmt = $dbh->prepare('DELETE FROM categories WHERE `id_categorie_categories` = :idCat');
+                    if (!$stmt->execute(['idCat' => $_POST['idCat']])) {
+                        echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
+                    } 
+                }
+            }
+        } else {
             echo "<span>Une erreur lors de la mise à jour du kanban a été detectée</span>";
         }
     } else {
